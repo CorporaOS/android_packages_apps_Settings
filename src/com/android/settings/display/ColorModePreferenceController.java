e0fdc1768568d912c6245dbe1b2858b6d0c4ddc8
daniml3 <danimoral1001@gmail.com>
2021-06-22 18:45:20 +0200

    Settings: hide the color mode preference if no color modes exist
    test: enable ro.surface_flinger.has_wide_color_display and observe
          that the preference is hidden when no color modes exist
    
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I746675572e515042683d09b51d0679ca5b118ad3

--- src/com/android/settings/display/ColorModePreferenceController.java
+++ src/com/android/settings/display/ColorModePreferenceController.java
@@ -31,10 +31,11 @@ public class ColorModePreferenceController extends BasePreferenceController {
 
     @Override
     public int getAvailabilityStatus() {
-        return mContext.getSystemService(ColorDisplayManager.class)
-                .isDeviceColorManaged()
-                && !ColorDisplayManager.areAccessibilityTransformsEnabled(mContext) ?
-                AVAILABLE : DISABLED_FOR_USER;
+        int colorModeCount = mContext.getResources().getIntArray(
+                com.android.internal.R.array.config_availableColorModes).length;
+        return mContext.getSystemService(ColorDisplayManager.class).isDeviceColorManaged()
+                && !ColorDisplayManager.areAccessibilityTransformsEnabled(mContext)
+                && (colorModeCount > 0) ? AVAILABLE : DISABLED_FOR_USER;
     }
 
     @Override
