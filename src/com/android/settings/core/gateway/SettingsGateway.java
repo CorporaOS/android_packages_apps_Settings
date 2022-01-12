cab93a1650d8ca420154b94e3a43b5faf72fb6c1
Eric Biggers <ebiggers@google.com>
2022-01-11 17:01:22 -0800

    Remove CryptKeeperSettings
    FDE support has been removed in favor of FBE, so remove the FDE settings
    from the "Encryption & credentials" page of the Settings app.
    
    For now I didn't change the way the page appears on devices that don't
    use FDE; as before, it still lists "Encrypt phone", followed by either
    "Encrypted" or "Phone not encrypted" with no changeable settings.  Note
    that the strings used for this have "crypt_keeper" in their names but
    aren't specific to FDE; the next CL will rename them.
    
    Test: On Cuttlefish with and without encryption enabled, tested visiting
          the "Encryption & credentials" settings.
    Bug: 208476087
    Change-Id: I3ce9894291ea1f1886f21980a86a92bfce38038a
    (cherry picked from commit 36609c18d115d7ed72a08ed1623fc118639c69d4)
    Merged-In: I3ce9894291ea1f1886f21980a86a92bfce38038a

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -141,7 +141,6 @@ import com.android.settings.password.ChooseLockPattern;
 import com.android.settings.print.PrintJobSettingsFragment;
 import com.android.settings.print.PrintSettingsFragment;
 import com.android.settings.privacy.PrivacyDashboardFragment;
-import com.android.settings.security.CryptKeeperSettings;
 import com.android.settings.security.LockscreenDashboardFragment;
 import com.android.settings.security.SecuritySettings;
 import com.android.settings.shortcut.CreateShortcut;
@@ -235,7 +234,6 @@ public class SettingsGateway {
             PickupGestureSettings.class.getName(),
             DoubleTwistGestureSettings.class.getName(),
             SystemNavigationGestureSettings.class.getName(),
-            CryptKeeperSettings.class.getName(),
             DataUsageSummary.class.getName(),
             DreamSettings.class.getName(),
             UserSettings.class.getName(),
