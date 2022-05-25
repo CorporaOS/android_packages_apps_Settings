080e968e5855dee902dbca6241334eb629c4b80d
Alice Kuo <aliceypkuo@google.com>
2022-05-25 13:49:59 +0800

    Add LE Audio feature switcher in the developer option menu
    Bug: 233018305
    Bug: 233005340
    Test: switch LE audio feature, and check LE audio functionality status
    Change-Id: I8adcf27edd1438df445d32fca93f35ff5020a3b3

--- tests/robotests/src/com/android/settings/development/BluetoothLeAudioPreferenceControllerTest.java
+++ tests/robotests/src/com/android/settings/development/BluetoothLeAudioPreferenceControllerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The Android Open Source Project
+ * Copyright (C) 2022 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,18 +16,16 @@
 
 package com.android.settings.development;
 
-import static com.android.settings.development.BluetoothA2dpHwOffloadPreferenceController
-        .A2DP_OFFLOAD_DISABLED_PROPERTY;
-import static com.android.settings.development.BluetoothLeAudioHwOffloadPreferenceController
-        .LE_AUDIO_OFFLOAD_DISABLED_PROPERTY;
-
-import static com.google.common.truth.Truth.assertThat;
-
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothStatusCodes;
 import android.content.Context;
-import android.os.SystemProperties;
 
 import androidx.preference.PreferenceScreen;
 import androidx.preference.SwitchPreference;
@@ -41,60 +39,60 @@ import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 
 @RunWith(RobolectricTestRunner.class)
-public class BluetoothA2dpHwOffloadPreferenceControllerTest {
+public class BluetoothLeAudioPreferenceControllerTest {
 
     @Mock
     private PreferenceScreen mPreferenceScreen;
     @Mock
     private DevelopmentSettingsDashboardFragment mFragment;
 
+    @Mock
+    private BluetoothAdapter mBluetoothAdapter;
+
     private Context mContext;
     private SwitchPreference mPreference;
-    private BluetoothA2dpHwOffloadPreferenceController mController;
+    private BluetoothLeAudioPreferenceController mController;
 
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
         mContext = RuntimeEnvironment.application;
         mPreference = new SwitchPreference(mContext);
-        mController = spy(new BluetoothA2dpHwOffloadPreferenceController(mContext, mFragment));
+        mController = spy(new BluetoothLeAudioPreferenceController(mContext, mFragment));
+        doNothing().when(mController).switchLeAudioProfileStatus(anyBoolean());
         when(mPreferenceScreen.findPreference(mController.getPreferenceKey()))
             .thenReturn(mPreference);
+        mController.mBluetoothAdapter = mBluetoothAdapter;
         mController.displayPreference(mPreferenceScreen);
     }
 
     @Test
-    public void onA2dpHwDialogConfirmedAsA2dpOffloadDisabled_shouldChangeProperty() {
-        SystemProperties.set(A2DP_OFFLOAD_DISABLED_PROPERTY, Boolean.toString(false));
+    public void onRebootDialogConfirmedAsLeAudioDisabled_shouldSwitchStatus() {
+        when(mBluetoothAdapter.isLeAudioSupported())
+                .thenReturn(BluetoothStatusCodes.FEATURE_NOT_SUPPORTED);
         mController.mChanged = true;
 
         mController.onRebootDialogConfirmed();
-        final boolean mode = SystemProperties.getBoolean(A2DP_OFFLOAD_DISABLED_PROPERTY, false);
-        assertThat(mode).isTrue();
+        verify(mController).switchLeAudioProfileStatus(true);
     }
 
     @Test
-    public void onA2dpHwDialogConfirmedAsA2dpOffloadEnabled_shouldChangeProperty() {
-        SystemProperties.set(A2DP_OFFLOAD_DISABLED_PROPERTY, Boolean.toString(true));
-        SystemProperties.set(LE_AUDIO_OFFLOAD_DISABLED_PROPERTY, Boolean.toString(true));
-
+    public void onRebootDialogConfirmedAsLeAudioEnabled_shouldSwitchStatus() {
+        when(mBluetoothAdapter.isLeAudioSupported())
+                .thenReturn(BluetoothStatusCodes.FEATURE_SUPPORTED);
         mController.mChanged = true;
 
         mController.onRebootDialogConfirmed();
-        final boolean a2dpMode = SystemProperties.getBoolean(A2DP_OFFLOAD_DISABLED_PROPERTY, true);
-        final boolean leAudioMode = SystemProperties
-                .getBoolean(LE_AUDIO_OFFLOAD_DISABLED_PROPERTY, true);
-        assertThat(a2dpMode).isFalse();
-        assertThat(leAudioMode).isFalse();
+        verify(mController).switchLeAudioProfileStatus(false);
     }
 
     @Test
-    public void onA2dpHwDialogCanceled_shouldNotChangeProperty() {
-        SystemProperties.set(A2DP_OFFLOAD_DISABLED_PROPERTY, Boolean.toString(false));
+    public void onRebootDialogCanceled_shouldNotSwitchStatus() {
+        when(mBluetoothAdapter.isLeAudioSupported())
+                .thenReturn(BluetoothStatusCodes.FEATURE_NOT_SUPPORTED);
         mController.mChanged = true;
 
         mController.onRebootDialogCanceled();
-        final boolean mode = SystemProperties.getBoolean(A2DP_OFFLOAD_DISABLED_PROPERTY, false);
-        assertThat(mode).isFalse();
+        verify(mController, never()).switchLeAudioProfileStatus(anyBoolean());
     }
 }
