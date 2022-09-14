5a7ddf011885580e0f76c110d5c1d6525d3f02f6
Viesturs Zarins <viesturs@google.com>
2022-09-14 15:43:48 +0000

    Move android.settings.NFC_SETTINGS intent handler to open the Nfc settings fragment, not the generic connected devices fragment.
    Change-Id: I0457730b7711cd760f6cfb4343f036d370cd3577
    Context: b/246772836

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -78,6 +78,7 @@ import com.android.settings.bluetooth.BluetoothPairingDetail;
 import com.android.settings.bugreporthandler.BugReportHandlerPicker;
 import com.android.settings.connecteddevice.AdvancedConnectedDeviceDashboardFragment;
 import com.android.settings.connecteddevice.ConnectedDeviceDashboardFragment;
+import com.android.settings.connecteddevice.NfcAndPaymentFragment;
 import com.android.settings.connecteddevice.PreviouslyConnectedDeviceDashboardFragment;
 import com.android.settings.connecteddevice.usb.UsbDetailsFragment;
 import com.android.settings.datausage.DataSaverSummary;
@@ -345,7 +346,8 @@ public class SettingsGateway {
             MobileNetworkSettings.class.getName(),
             AppLocaleDetails.class.getName(),
             TurnScreenOnSettings.class.getName(),
-            TurnScreenOnDetails.class.getName()
+            TurnScreenOnDetails.class.getName(),
+            NfcAndPaymentFragment.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
