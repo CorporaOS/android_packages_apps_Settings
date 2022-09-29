b1abb11c81d589a3eebde8c9fd2aa006ce33c209
xuelin <xuelin@xiaomi.corp-partner.google.com>
2022-09-29 16:23:43 +0800

    Fix Security mismatch in CTS-network request tests
    In the Network Request with a SSID and BSSID pattern
    test, when connecting to an AP with psk/sae hybrid
    encryption, the Wifi security obtained by
    getSecurity() and getWifiEntrySecurity(matchedScanResult)
    does not match, resulting in the failure of the ctsVerifier.
    
    Bug:
    Test:com.android.cts.verifier#NetworkRequestPatternNetworkSpecifierTest
    
    Signed-off-by: xuelin <xuelin@xiaomi.corp-partner.google.com>
    Change-Id: Idd3a2228f8c57ece65fe149fa0aa7258e464f596

--- src/com/android/settings/wifi/NetworkRequestDialogFragment.java
+++ src/com/android/settings/wifi/NetworkRequestDialogFragment.java
@@ -17,6 +17,7 @@
 package com.android.settings.wifi;
 
 import static com.android.settings.wifi.WifiUtils.getWifiEntrySecurity;
+import static com.android.wifitrackerlib.Utils.getSecurityTypesFromScanResult;
 
 import static java.util.stream.Collectors.toList;
 
@@ -277,8 +278,13 @@ public class NetworkRequestDialogFragment extends NetworkRequestDialogBaseFragme
         mFilteredWifiEntries.clear();
         mFilteredWifiEntries.addAll(wifiEntries.stream().filter(entry -> {
             for (ScanResult matchedScanResult : mMatchedScanResults) {
+                final List<Integer> entrySecurityTypes = entry.getSecurityTypes();
+                final List<Integer> matchedSecurityTypes =
+                        getSecurityTypesFromScanResult(matchedScanResult);
+                final List<Integer> intersectionSecurityTypes = entrySecurityTypes.stream()
+                        .filter(matchedSecurityTypes::contains).collect(toList());
                 if (TextUtils.equals(entry.getSsid(), matchedScanResult.SSID)
-                        && entry.getSecurity() == getWifiEntrySecurity(matchedScanResult)) {
+                        && intersectionSecurityTypes.size() > 0) {
                     return true;
                 }
             }
