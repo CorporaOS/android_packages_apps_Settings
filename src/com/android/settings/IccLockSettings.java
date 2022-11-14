4ccddd437a935f9dc44216df7c70a54cc01dd6fc
Bonian Chen <bonianchen@google.com>
2022-11-14 09:39:22 +0000

    [Settings] configuration for hidding SIM lock UI
    Hide SIM lock UI based on configuraion.
    
    Bug: 240515161
    Test: test cases and local testing
    Merged-In: I870c0b53112db56b7bc80bfd585f6f7b3cf82737
    Change-Id: I70be07dbc1decb5cb8eff384d4cb8bea355ab99d

--- src/com/android/settings/IccLockSettings.java
+++ src/com/android/settings/IccLockSettings.java
@@ -174,7 +174,8 @@ public class IccLockSettings extends SettingsPreferenceFragment
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (Utils.isMonkeyRunning()) {
+        if (Utils.isMonkeyRunning() ||
+                !SubscriptionUtil.isSimHardwareVisible(getContext())) {
             finish();
             return;
         }
