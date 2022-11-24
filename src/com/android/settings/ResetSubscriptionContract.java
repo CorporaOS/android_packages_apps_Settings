5b05b8eb2d6aa0cfdbea457c8c479a9bad0cd9b9
Bonian Chen <bonianchen@google.com>
2022-11-24 15:23:07 +0000

    [Settings] Code refactor for SIM change detection
    Code refactor for maintainance.
    
    Bug: 259611847
    Test: local test
    Change-Id: I64cbcef34d63486f0add1bffc5ca5165e373d93b

---
+++ src/com/android/settings/ResetSubscriptionContract.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings;
+
+import android.content.Context;
+import android.telephony.SubscriptionManager;
+import android.telephony.SubscriptionManager.OnSubscriptionsChangedListener;
+import android.util.Log;
+
+import androidx.annotation.VisibleForTesting;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.IntStream;
+
+/**
+ * A Class monitoring the availability of subscription IDs provided within reset request.
+ *
+ * This is to detect the situation when user changing SIM card during the presenting of
+ * confirmation UI.
+ */
+public class ResetSubscriptionContract implements AutoCloseable {
+    private static final String TAG = "ResetSubscriptionContract";
+
+    private final Context mContext;
+    private ExecutorService mExecutorService;
+    private final int [] mResetSubscriptionIds;
+    @VisibleForTesting
+    protected OnSubscriptionsChangedListener mSubscriptionsChangedListener;
+    private AtomicBoolean mSubscriptionsUpdateNotify = new AtomicBoolean();
+
+    /**
+     * Constructor
+     * @param context
+     * @param resetRequest the request object for perform network reset operation.
+     */
+    public ResetSubscriptionContract(Context context,
+            ResetNetworkRequest resetRequest) {
+        mContext = context;
+        // Only keeps specific subscription ID required to perform reset operation
+        IntStream subIdStream = IntStream.of(
+                resetRequest.getResetTelephonyAndNetworkPolicyManager()
+                , resetRequest.getResetApnSubId());
+        mResetSubscriptionIds = subIdStream.sorted().distinct()
+                .filter(id -> SubscriptionManager.isUsableSubscriptionId(id))
+                .toArray();
+
+        if (mResetSubscriptionIds.length <= 0) {
+            return;
+        }
+
+        mExecutorService = Executors.newSingleThreadExecutor();
+        startMonitorSubscriptionChange();
+    }
+
+    /**
+     * A method for detecting if there's any subscription under monitor no longer active.
+     * @return subscription ID which is no longer active.
+     */
+    public Integer getMissingSubscriptionId() {
+        if (mResetSubscriptionIds.length <= 0) {
+            return null;
+        }
+        SubscriptionManager mgr = getSubscriptionManager();
+        if (mgr == null) {
+            Log.w(TAG, "Fail to access subscription manager");
+            return mResetSubscriptionIds[0];
+        }
+        for (int idx=0; idx<mResetSubscriptionIds.length; idx++) {
+            int subId = mResetSubscriptionIds[idx];
+            if (mgr.getActiveSubscriptionInfo(subId) == null) {
+                Log.w(TAG, "SubId " + subId + " no longer active.");
+                return subId;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Async callback when detecting if there's any subscription under monitor no longer active.
+     * @param subscriptionId subscription ID which is no longer active.
+     */
+    public void onSubscriptionInActive(int subscriptionId) {}
+
+    @VisibleForTesting
+    protected SubscriptionManager getSubscriptionManager() {
+        return mContext.getSystemService(SubscriptionManager.class);
+    }
+
+    private void startMonitorSubscriptionChange() {
+        SubscriptionManager mgr = getSubscriptionManager();
+        if (mgr == null) {
+            return;
+        }
+        // update monitor listener
+        mSubscriptionsChangedListener = new OnSubscriptionsChangedListener() {
+            @Override
+            public void onSubscriptionsChanged() {
+                mSubscriptionsUpdateNotify.set(true);
+
+                // Back to main UI thread
+                mContext.getMainExecutor().execute(() -> {
+                    // Remove notifications and perform checking.
+                    if (mSubscriptionsUpdateNotify.getAndSet(false)) {
+                        Integer subId = getMissingSubscriptionId();
+                        if (subId != null) {
+                            onSubscriptionInActive(subId);
+                        }
+                    }
+                });
+            }
+        };
+
+        mgr.addOnSubscriptionsChangedListener(
+                mExecutorService, mSubscriptionsChangedListener);
+    }
+
+    // Implementation of AutoCloseable
+    public void close() {
+        if (mExecutorService == null) {
+            return;
+        }
+        // Stop monitoring subscription change
+        SubscriptionManager mgr = getSubscriptionManager();
+        if (mgr != null) {
+            mgr.removeOnSubscriptionsChangedListener(mSubscriptionsChangedListener);
+        }
+        // Release Executor
+        mExecutorService.shutdownNow();
+        mExecutorService = null;
+    }
+}
