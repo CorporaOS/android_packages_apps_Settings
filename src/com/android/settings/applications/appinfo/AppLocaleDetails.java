bcefd2165f310c307b49280703a1d11729432835
hee.youn <hee.youn@samsung.com>
2022-11-25 18:02:16 +0900

    modify to unifying display name of locale because there is difference between summary and AppLanguageList
    Change-Id: I62b91a33632251e810d200d56d7274e216568c77

--- src/com/android/settings/applications/appinfo/AppLocaleDetails.java
+++ src/com/android/settings/applications/appinfo/AppLocaleDetails.java
@@ -213,7 +213,7 @@ public class AppLocaleDetails extends SettingsPreferenceFragment {
         if (appLocale == null) {
             return context.getString(R.string.preference_of_system_locale_summary);
         } else {
-            return appLocale.getDisplayName(appLocale);
+            return LocaleHelper.getDisplayName(appLocale, appLocale, true);
         }
     }
 }
