a4d8c78ac70ae32a0d74e86f6a99f115beca451d
Bonian Chen <bonianchen@google.com>
2022-11-28 09:27:43 +0000

    [Settings] configuration for hidding SIM remove UI
    Hide SIM remove UI based on configuraion.
    
    Bug: 240515161
    Test: test cases and local testing
    Change-Id: I627be682d64228deb373fc7fa5d79c42a6be8241

--- src/com/android/settings/ResetNetwork.java
+++ src/com/android/settings/ResetNetwork.java
@@ -251,6 +251,9 @@ public class ResetNetwork extends InstrumentedFragment {
     }
 
     private List<SubscriptionInfo> getActiveSubscriptionInfoList() {
+        if (!SubscriptionUtil.isSimHardwareVisible(getActivity())) {
+            return Collections.emptyList();
+        }
         SubscriptionManager mgr = getActivity().getSystemService(SubscriptionManager.class);
         if (mgr == null) {
             Log.w(TAG, "No SubscriptionManager");
@@ -280,6 +283,9 @@ public class ResetNetwork extends InstrumentedFragment {
     }
 
     private boolean showEuiccSettings(Context context) {
+        if (!SubscriptionUtil.isSimHardwareVisible(context)) {
+            return false;
+        }
         EuiccManager euiccManager =
                 (EuiccManager) context.getSystemService(Context.EUICC_SERVICE);
         if (!euiccManager.isEnabled()) {
