e789ccb822479d682ab924cdcc0f20c64bbe1f95
Yuchen Dong <yuchen.x.dong@sony.com>
2023-03-15 16:59:25 +0800

    Prevent interacting with smart forwarding on Single SIM
    Even if one SIM is removed after enabling feature on the smart
    forwarding screen, the user can disable the feature on the remaining
    SIM side, but the removed SIM will still have the settings.
    
    Add check for smart forwarding to support only if device has least 2
    SIMs active.
    
    Test: manual
    Bug: 285080795
    Change-Id: Ia321896ee629fb20a887087a92723488dfb9eb6b

--- src/com/android/settings/sim/smartForwarding/SmartForwardingUtils.java
+++ src/com/android/settings/sim/smartForwarding/SmartForwardingUtils.java
@@ -23,6 +23,9 @@ import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SmartForwardingUtils {
     public static final String TAG = "SmartForwarding";
     public static final String SMART_FORWARDING_PREF = "smart_forwarding_pref_";
@@ -141,5 +144,16 @@ public class SmartForwardingUtils {
         } else {
             return "";
         }
+    }
+
+    /**
+     * Check if device has at least 2 active SIMs to support SmartForwarding.
+     */
+    public static boolean onDualSim(Context context) {
+        SubscriptionManager subscriptionManager = context.getSystemService(
+                SubscriptionManager.class);
+        return SubscriptionUtil.getActiveSubscriptions(subscriptionManager).stream()
+                .filter(subInfo -> !subInfo.isOpportunistic())
+                .count() >= 2;
     }
 }
