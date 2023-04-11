f2b8bdd6ea5fc7993ab87cd1bf17ec03e4d39395
chiachangwang <chiachangwang@google.com>
2023-04-11 02:23:56 +0000

    Populate ipsecSecret field in VpnProfile
    This commit fixes IKEv2 RSA VPN could not be an always-on VPN
    issue to populate ipsecSecret field from "IPsec user certificate".
    
    Fields ipsecUserCert and ipsecSecret are required for a valid
    VpnProfile. The ipsecSecret field in VpnProfile should be
    populated from "IPsec user certificate" field. It's the matching
    private key.
    
    Bug: 263610164
    Bug: 230727391
    Test: Verify if IKEv2 RSA VPN can setup successfully and set as
          as always on VPN
    Change-Id: I605b35fff9e399ee7b82d8441137984992ab2907

--- src/com/android/settings/vpn2/ConfigDialog.java
+++ src/com/android/settings/vpn2/ConfigDialog.java
@@ -656,7 +656,12 @@ class ConfigDialog extends AlertDialog implements TextWatcher,
             case VpnProfile.TYPE_L2TP_IPSEC_RSA:
                 profile.l2tpSecret = mL2tpSecret.getText().toString();
                 // fall through
-            case VpnProfile.TYPE_IKEV2_IPSEC_RSA: // fall through
+            case VpnProfile.TYPE_IKEV2_IPSEC_RSA:
+                if (mIpsecUserCert.getSelectedItemPosition() != 0
+                        && profile.type == VpnProfile.TYPE_IKEV2_IPSEC_RSA) {
+                    profile.ipsecSecret = (String) mIpsecUserCert.getSelectedItem();
+                }
+                // fall through
             case VpnProfile.TYPE_IPSEC_XAUTH_RSA:
                 if (mIpsecUserCert.getSelectedItemPosition() != 0) {
                     profile.ipsecUserCert = (String) mIpsecUserCert.getSelectedItem();
