8fdfd2b370a2c3e43ab33e93bc634a514475c634
alisa.li <alisa.li@unisoc.com>
2023-08-09 13:47:15 +0800

    Update state when disabling and enabing develop option
    When not support a2dp offload, the a2dp offload preference is shown in grey, but a2dp offload preference will be set enabled when enabing develop option
    
    Bug:
    Test: manual
    Change-Id: I7e1d073ff9ef6b68b5f1d9faa1dac459a70d7afc

--- src/com/android/settings/development/BluetoothA2dpHwOffloadPreferenceController.java
+++ src/com/android/settings/development/BluetoothA2dpHwOffloadPreferenceController.java
@@ -73,6 +73,12 @@ public class BluetoothA2dpHwOffloadPreferenceController extends DeveloperOptions
         }
     }
 
+    @override
+    protected void onDeveloperOptionsSwitchEnabled() {
+	super.onDeveloperOptionsSwitchEnabled()
+        updateState(mPreference);
+    }
+
     @Override
     protected void onDeveloperOptionsSwitchDisabled() {
         super.onDeveloperOptionsSwitchDisabled();
