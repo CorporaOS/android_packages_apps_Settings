4b572211b509d640244eb95639074a200864ff4b
Pawan Wagh <waghpawan@google.com>
2023-08-11 21:36:38 +0000

    Developer option for booting with 16K pages
    Adding developer option to boot with 16K compatible kernel. Adding
    resources, layouts and dialog classes for 16k option. This needs
    to integrated with update engine to make OTA switch happen.
    
    Test: m Settings && adb install -r $ANDROID_PRODUCT_OUT/system_ext/priv-app/Settings/Settings.apk
    Bug: 295035851
    Bug: 294614538
    Change-Id: I3a24fed059eee99407e3486a7254285e0e4a251c

---
+++ src/com/android/settings/development/Enable16kPagesPreferenceController.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.development;
+
+import android.content.Context;
+import android.os.SystemProperties;
+import android.provider.Settings;
+import android.widget.Toast;
+
+import androidx.preference.Preference;
+import androidx.preference.SwitchPreference;
+
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.development.DeveloperOptionsPreferenceController;
+
+/**
+ *  Controller for 16K pages developer option
+ */
+public class Enable16kPagesPreferenceController extends DeveloperOptionsPreferenceController
+        implements Preference.OnPreferenceChangeListener, PreferenceControllerMixin {
+
+    private static final String ENABLE_16K_PAGES = "enable_16k_pages";
+    private static final String DEV_OPTION_PROPERTY = "ro.build.enable_16k_dev_option";
+    public static final int ENABLE_4K_PAGE_SIZE = 0;
+    public static final int ENABLE_16K_PAGE_SIZE = 1;
+
+    private final DevelopmentSettingsDashboardFragment mFragment;
+
+    public Enable16kPagesPreferenceController(
+            Context context, DevelopmentSettingsDashboardFragment fragment) {
+        super(context);
+        mFragment = fragment;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return SystemProperties.getBoolean(DEV_OPTION_PROPERTY, false);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return ENABLE_16K_PAGES;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final boolean optionEnabled = (Boolean) newValue;
+        Settings.Global.putInt(
+                mContext.getContentResolver(),
+                Settings.Global.ENABLE_16K_PAGES,
+                optionEnabled ? ENABLE_16K_PAGE_SIZE : ENABLE_4K_PAGE_SIZE);
+        if (optionEnabled) {
+            Enable16kPagesWarningDialog.show(mFragment);
+        } else {
+            // TODO(b/295573133):Directly reboot into 4k
+        }
+        return true;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        final int optionValue =
+                Settings.Global.getInt(
+                        mContext.getContentResolver(),
+                        Settings.Global.ENABLE_16K_PAGES,
+                        ENABLE_4K_PAGE_SIZE /* default */);
+
+        ((SwitchPreference) mPreference).setChecked(optionValue == ENABLE_16K_PAGE_SIZE);
+    }
+
+    @Override
+    protected void onDeveloperOptionsSwitchDisabled() {
+        super.onDeveloperOptionsSwitchDisabled();
+        Settings.Global.putInt(
+                mContext.getContentResolver(),
+                Settings.Global.ENABLE_16K_PAGES,
+                ENABLE_4K_PAGE_SIZE);
+        ((SwitchPreference) mPreference).setChecked(false);
+    }
+
+    /**
+     *  Called when user confirms to reboot with 16K pages
+     */
+    public void on16kDialogConfirmed() {
+        // TODO(b/295573133) : Remove toast and integrate update engine
+        Toast.makeText(mContext, "Rebooting with 16k kernel", Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     *  Called when user dismisses to reboot with 16K pages
+     */
+    public void on16kDialogDismissed() {}
+}
