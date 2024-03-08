ed91683bbc6b6b0b0a0c18952188e6f3bb42c366
hoffc <quic_hoffc@quicinc.com>
2024-03-08 17:31:04 +0800

    Settings: Fix force close for telephony callback exceeding limitation
    When sub info changed, new telephony callback will be registered but old telephony callback not un-registered. Finally result in force close for registered telephony callback count exceeding its allowed.
    
    No need to re-registered the telephony callback for one sub if registered.
    
    Buganizer: 328716180
    
    Change-Id: Ia0c3b311b5b5cc77f061d81ec21250abde2fa9f4

--- src/com/android/settings/network/MobileNetworkRepository.java
+++ src/com/android/settings/network/MobileNetworkRepository.java
@@ -245,7 +245,8 @@ public class MobileNetworkRepository extends SubscriptionManager.OnSubscriptions
     }
 
     private void createTelephonyManagerBySubId(int subId) {
-        if (subId == SubscriptionManager.INVALID_SUBSCRIPTION_ID) {
+        if (subId == SubscriptionManager.INVALID_SUBSCRIPTION_ID
+                || mTelephonyCallbackMap.containsKey(subId)) {
             return;
         }
         PhoneCallStateTelephonyCallback
