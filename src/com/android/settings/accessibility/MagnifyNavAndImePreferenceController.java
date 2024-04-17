cdf2644389925209bad588635341a72493ae38cb
susalin <susalin@amazon.com>
2024-04-17 22:46:23 +0000

    Add Option to Magnify Navigation Bar and IME (Pt.2)
    Users who use screen magnifier now have the option to magnify
    their navigation bar and IME (e.g. keyboard). We implement this through a
    new Secure Setting monitored through the AccessibilityManagerService,
    which can be toggled by a user in the Settings.
    
    Test: Manual
    Navigate to Settings > Accessibility > Magnification
    and turn on Magnify Navigation Bar and Keyboards .
    Test that the navigation bar and keyboard can be magnified
    depending on the setting. Turn the setting off, and the nav bar
    and keyboard should not be magnifiable.
    
    Change-Id: I880da997a7f23b746bc0ef3b4b5f072b7648d311

---
+++ src/com/android/settings/accessibility/MagnifyNavAndImePreferenceController.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.accessibility;
+
+import static com.android.settings.accessibility.AccessibilityUtil.State.OFF;
+import static com.android.settings.accessibility.AccessibilityUtil.State.ON;
+
+import android.content.Context;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.OnLifecycleEvent;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.SwitchPreference;
+
+import com.android.settings.R;
+import com.android.settings.core.TogglePreferenceController;
+
+/** Controller that accesses and switches
+ * the preference status of magnifying the nav bar and IME feature
+ */
+public class MagnifyNavAndImePreferenceController extends TogglePreferenceController
+        implements LifecycleObserver {
+
+    private static final String TAG =
+            MagnifyNavAndImePreferenceController.class.getSimpleName();
+    static final String PREF_KEY = "accessibility_magnify_nav_and_ime";
+
+    private SwitchPreference mMagnifyNavImePreference;
+
+    public MagnifyNavAndImePreferenceController(Context context, String preferenceKey) {
+        super(context, preferenceKey);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public boolean isChecked() {
+        return Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                Settings.Secure.ACCESSIBILITY_MAGNIFY_NAV_AND_IME,
+                OFF, UserHandle.USER_CURRENT) == ON;
+    }
+
+    @Override
+    public boolean setChecked(boolean isChecked) {
+        return Settings.Secure.putIntForUser(mContext.getContentResolver(),
+                Settings.Secure.ACCESSIBILITY_MAGNIFY_NAV_AND_IME,
+                (isChecked ? ON : OFF), UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return R.string.menu_key_accessibility;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mMagnifyNavImePreference = screen.findPreference(getPreferenceKey());
+    }
+
+    // TODO(b/186731461): Remove it when this controller is used in DashBoardFragment only.
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    void onResume() {
+        updateState();
+    }
+
+    /**
+     * Updates the state of preference components which has been displayed by
+     * {@link MagnifyNavAndImePreferenceController#displayPreference}.
+     */
+    void updateState() {
+        updateState(mMagnifyNavImePreference);
+    }
+}
