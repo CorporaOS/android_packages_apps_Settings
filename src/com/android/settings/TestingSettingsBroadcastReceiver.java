a3b8f7329c2d2360d0f85d06de77e191d578b031
Munikrishna J <jmunikrishna@google.com>
2024-06-14 07:20:05 +0000

    Test CL
    Change-Id: Id210a6a7d4c3026464119d3c07610e0877046661

--- src/com/android/settings/TestingSettingsBroadcastReceiver.java
+++ src/com/android/settings/TestingSettingsBroadcastReceiver.java
@@ -25,6 +25,8 @@ import com.android.settings.Settings.TestingSettingsActivity;
 
 
 public class TestingSettingsBroadcastReceiver extends BroadcastReceiver {
+    private static final String ACTION_ESOS_TEST =
+            "com.google.android.apps.stargate.ACTION_ESOS_QUESTIONNAIRE";
 
     public TestingSettingsBroadcastReceiver() {
     }
@@ -33,9 +35,9 @@ public class TestingSettingsBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if (intent != null && intent.getAction() != null
                 && intent.getAction().equals(TelephonyManager.ACTION_SECRET_CODE)) {
-            Intent i = new Intent(Intent.ACTION_MAIN);
-            i.setClass(context, TestingSettingsActivity.class);
-            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            //Intent i = new Intent(Intent.ACTION_MAIN);
+            Intent i = new Intent(ACTION_ESOS_TEST);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             context.startActivity(i);
         }
     }
