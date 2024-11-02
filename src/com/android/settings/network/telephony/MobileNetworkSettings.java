dc5a1bd9857f2647368950995de6581d8998c593
techyminati <sinha.aryan03@gmail.com>
2024-11-02 12:09:29 +0530

    Re-add "Backup Calling" toggle to Settings UI
    * The relevant code already exists on AOSP, Apart from that this is a very useful feature, I have no idea why it was removed in 812b59b981ddb2b09e44880424ad3e010fbecc08 , Many OEMs already support this, hence expose this toggle to users again.
    
    This reverts commit 812b59b981ddb2b09e44880424ad3e010fbecc08.
    
    Change-Id: I01bf14e3150f813c128ed28605ed534699a0a03e
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- src/com/android/settings/network/telephony/MobileNetworkSettings.java
+++ src/com/android/settings/network/telephony/MobileNetworkSettings.java
@@ -301,6 +301,11 @@ public class MobileNetworkSettings extends AbstractMobileNetworkSettings impleme
         final VideoCallingPreferenceController videoCallingPreferenceController =
                 use(VideoCallingPreferenceController.class)
                         .init(mSubId, callingPreferenceCategoryController);
+        final BackupCallingPreferenceController crossSimCallingPreferenceController =
+                use(BackupCallingPreferenceController.class).init(mSubId);
+        use(CallingPreferenceCategoryController.class).setChildren(
+                Arrays.asList(wifiCallingPreferenceController, videoCallingPreferenceController,
+                        crossSimCallingPreferenceController));
         use(Enhanced4gLtePreferenceController.class).init(mSubId)
                 .addListener(videoCallingPreferenceController);
         use(Enhanced4gCallingPreferenceController.class).init(mSubId)
