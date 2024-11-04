5c6e1dafa03830637bb13ee0727adf8629dd4994
Jaewan Kim <jaewan@google.com>
2024-11-05 03:39:08 +0900

    Clear Linux terminal app when disabled
    Bug: 374032310
    Test: Manual, T/H
    Flag: Build.RELEASE_AVF_SUPPORT_CUSTOM_VM_WITH_PARAVIRTUALIZED_DEVICES
    Change-Id: Id56f98cf90b0598111baa3096b1b81307ebb99a3

--- tests/robotests/src/com/android/settings/development/linuxterminal/EnableLinuxTerminalPreferenceControllerTest.java
+++ tests/robotests/src/com/android/settings/development/linuxterminal/EnableLinuxTerminalPreferenceControllerTest.java
@@ -138,6 +138,8 @@ public class EnableLinuxTerminalPreferenceControllerTest {
                         mTerminalPackageName,
                         PackageManager.COMPONENT_ENABLED_STATE_DEFAULT,
                         /* flags= */ 0);
+
+        verify(mPackageManager).clearApplicationUserData(eq(mTerminalPackageName), any());
     }
 
     @Test
