3c35825e4fe9b6d3e01b5836ad07a2cfe165e627
Jaewan Kim <jaewan@google.com>
2024-11-04 15:25:53 +0900

    Make Linux terminal option profile aware
    Bug: 374034911
    Test: atest, plus following manual test \
      - Test tabbed UI with/without work profile \
      - Test that disabled by work profile launches alert dialog \
      - Test whether toggling an app only toggle the app for the user.
    Flag: RELEASE_AVF_SUPPORT_CUSTOM_VM_WITH_PARAVIRTUALIZED_DEVICES
    Change-Id: I4bf0a2d521cf3e632f6c0320e0b5cc0154d5b68f

---
+++ src/com/android/settings/development/linuxterminal/EnableLinuxTerminalPreferenceController.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.development.linuxterminal;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.UserHandle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.CompoundButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settings.widget.SettingsMainSwitchPreference;
+import com.android.settingslib.RestrictedLockUtils.EnforcedAdmin;
+
+/** Preference controller for enable/disable toggle of the linux terminal */
+public class EnableLinuxTerminalPreferenceController extends BasePreferenceController
+        implements CompoundButton.OnCheckedChangeListener, PreferenceControllerMixin {
+    private static final String TAG = "LinuxTerminalPrefCtrl";
+
+    private static final String ENABLE_TERMINAL_KEY = "enable_linux_terminal";
+
+    @NonNull private final PackageManager mPackageManager;
+    private final int mUserId;
+    @Nullable private final String mTerminalPackageName;
+
+    private SettingsMainSwitchPreference mPreference;
+
+    public EnableLinuxTerminalPreferenceController(
+            @NonNull Context context, @NonNull Context userAwareContext, int userId) {
+        super(context, ENABLE_TERMINAL_KEY);
+
+        mPackageManager = userAwareContext.getPackageManager();
+        mUserId = userId;
+
+        String packageName = mContext.getString(R.string.config_linux_terminal_app_package_name);
+        mTerminalPackageName =
+                isPackageInstalled(mPackageManager, packageName) ? packageName : null;
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public void displayPreference(@NonNull PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mPreference = screen.findPreference(getPreferenceKey());
+        mPreference.addOnSwitchChangeListener(this);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        String packageName = getTerminalPackageName();
+        if (packageName == null) {
+            return;
+        }
+
+        int state =
+                isChecked
+                        ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+                        : PackageManager.COMPONENT_ENABLED_STATE_DEFAULT;
+        mPackageManager.setApplicationEnabledSetting(packageName, state, /* flags= */ 0);
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        String packageName = getTerminalPackageName();
+        boolean isInstalled = (packageName != null);
+        if (isInstalled) {
+            mPreference.setDisabledByAdmin(/* EnforcedAdmin= */ null);
+            mPreference.setEnabled(/* enabled= */ true);
+            boolean terminalEnabled =
+                    mPackageManager.getApplicationEnabledSetting(packageName)
+                            == PackageManager.COMPONENT_ENABLED_STATE_ENABLED;
+            mPreference.setChecked(terminalEnabled);
+        } else {
+            if (isPrimaryUser()) {
+                Log.e(TAG, "Terminal app doesn't exist for primary user");
+                mPreference.setDisabledByAdmin(/* EnforcedAdmin= */ null);
+                mPreference.setEnabled(/* enabled= */ false);
+            } else {
+                // If admin hasn't enabled the system app, mark it as disabled by admin.
+                mPreference.setDisabledByAdmin(new EnforcedAdmin());
+                // Make it enabled, so clicking it would show error dialog.
+                mPreference.setEnabled(/* enabled= */ true);
+            }
+            mPreference.setChecked(/* checked= */ false);
+        }
+    }
+
+    // Can be mocked for testing
+    @VisibleForTesting
+    @Nullable
+    String getTerminalPackageName() {
+        return mTerminalPackageName;
+    }
+
+    @VisibleForTesting
+    boolean isPrimaryUser() {
+        return mUserId == UserHandle.myUserId();
+    }
+
+    private static boolean isPackageInstalled(PackageManager manager, String packageName) {
+        if (TextUtils.isEmpty(packageName)) {
+            return false;
+        }
+        try {
+            return manager.getPackageInfo(
+                            packageName,
+                            PackageManager.MATCH_ALL | PackageManager.MATCH_DISABLED_COMPONENTS)
+                    != null;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+}
